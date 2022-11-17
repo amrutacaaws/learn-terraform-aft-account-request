dc727e9591732c44d7bcd58f7a757dc65ad269ae
Amruta Chitloor Anjaneyalu <amrutaca@amazon.com>
2022-11-17 12:47:14 -0500

    Added VPC CIDR

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,10 @@ module "second_sandbox" {
 
   custom_fields = {
     group = "non-prod"
+    azs = "us-east-1a,us-east-1b,us-east-1c"
+    vpc_cidr = "10.0.0.0/16"
+    vpc_private_subnets = "10.0.1.0/24,10.0.2.0/24,10.0.3.0/24"
+    vpc_public_subnets = "10.0.101.0/24,10.0.102.0/24,10.0.103.0/24"
   }
 
   account_customizations_name = "second_sandbox"
